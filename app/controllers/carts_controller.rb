29449782b919cc712f37973d014715f5a6aadf70
Danilo Castro <danilogcastro@hotmail.com>
2025-04-05 12:16:20 -0300

    rotas e métodos provisórios

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,16 @@
 class CartsController < ApplicationController
   ## TODO Escreva a lógica dos carrinhos aqui
+
+  def show
+    render json: { PING: 'PONG' }
+  end
+
+  def update
+    render json: { PING: 'PONG' }
+  end
+
+  # QUAL É EXATAMENTE O SENTIDO DESSE ENDPOINT?
+  def add_item
+    'PONG'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  get '/cart', to: 'carts#show'
+  patch '/cart', to: 'carts#update'
+  patch '/cart/add_item', to: 'carts#add_item'
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
