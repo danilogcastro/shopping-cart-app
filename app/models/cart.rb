f70e3f4e5363a46d3a51a066305492fc8ead902a
Danilo Castro <danilogcastro@hotmail.com>
2025-04-05 23:55:23 -0300

    callback para atualizar valor total do carrinho

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,10 +4,6 @@ class Cart < ApplicationRecord
 
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
-  # CONSIDERO UM POUCO PROBLEMÁTICO TER UMA COLUNA total_price QUE É DEPENDENTE
-  # DE DADOS CONTIDOS EM OUTRA TABELA, E QUE PRECISARIA SER ATUALIZADA
-  # TODA VEZ QUE UM NOVO ITEM É ADICIONADO AO CARRINHO, QUALQUER ERRO
-  # PODENDO LEVAR A INCONSISTÊNCIAS
   def calculate_total
     items.joins(:product).sum('quantity * products.price')
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,14 @@ class Item < ApplicationRecord
   belongs_to :product
 
   validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+
+  after_commit :update_cart_total
+
+  # CONSIDERO UM POUCO PROBLEMÁTICO TER UMA COLUNA total_price QUE É DEPENDENTE
+  # DE DADOS CONTIDOS EM OUTRA TABELA, E QUE PRECISARIA SER ATUALIZADA
+  # TODA VEZ QUE UM NOVO ITEM É ADICIONADO AO CARRINHO, QUALQUER ERRO
+  # PODENDO LEVAR A INCONSISTÊNCIAS
+  def update_cart_total
+    cart.update(total_price: cart.calculate_total)
+  end
 end
